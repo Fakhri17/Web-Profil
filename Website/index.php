4d4b88d11168b385d3d1b5b2b4ed28f0df199b78
Fakhri Alauddin <fakhricoffe@gmail.com>
2019-09-17 19:55:01 +0700

    Update array and form contact

--- Website/index.php
+++ Website/index.php
@@ -18,9 +18,49 @@
         ] ];
 
         $profile = [ [
+           'nama' => 'Fakhri Alauddin',
+           'age' => '18 Tahun',
+           'ttl' => 'Jombang, 31 Maret 2001',
+           'alamat' => 'Jl Raya Benowo No 06',
+           'hoby' => " Coding dan Editing",
            
-          
         ] ];
+
+        $skill = [ [ 
+
+          'web' => ' HTML CSS JS',
+          'data' => 'JAVA PHP C++ '
+           
+        ] ];
+
+        $education = [ 
+          [ 
+            'year' => '2009 - 2015',
+            'deggre' => 'Siswa',
+            'scholl' => 'SDN Benowo 1',
+            'location' => 'Jl Raya Benowo No 71, Pakal, Surabaya',
+        ],
+        [
+            'year' => '2015 - 2018',
+            'deggre' => 'Siswa',
+            'scholl' => 'SMPN 14 Surabaya',
+            'location' => 'Jl. Jurang Kuping, Pakal, Surabaya',
+
+        ],
+        [
+          'year' => '2015 - Sekarang',
+          'deggre' => 'Siswa',
+          'scholl' => 'SMKN 2  Surabaya',
+          'location' => 'Jl. Tentara Genie Pelajar No.26, Sawahan, Surabaya',
+
+      ],
+
+    ];
+
+       $contact [ [
+
+       ] ];
+         
         ?>
 
 
@@ -65,6 +105,93 @@
           <!-- Profile -->
           <!-- End Profile -->
 
+
+        <!-- Contact -->
+        <div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Text input">
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Username</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+  <p class="help is-success">This username is available</p>
+</div>
+
+<div class="field">
+  <label class="label">Email</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+  </div>
+  <p class="help is-danger">This email is invalid</p>
+</div>
+
+<div class="field">
+  <label class="label">Subject</label>
+  <div class="control">
+    <div class="select">
+      <select>
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Message</label>
+  <div class="control">
+    <textarea class="textarea" placeholder="Textarea"></textarea>
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <label class="checkbox">
+      <input type="checkbox">
+      I agree to the <a href="#">terms and conditions</a>
+    </label>
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <label class="radio">
+      <input type="radio" name="question">
+      Yes
+    </label>
+    <label class="radio">
+      <input type="radio" name="question">
+      No
+    </label>
+  </div>
+</div>
+
+<div class="field is-grouped">
+  <div class="control">
+    <button class="button is-link">Submit</button>
+  </div>
+  <div class="control">
+    <button class="button is-text">Cancel</button>
+  </div>
+</div>
+        <!-- Contact -->
           <!-- Footer -->
           <?php include 'footer.php'; ?>
           <!-- End Footer -->
