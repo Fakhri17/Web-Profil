93833c7bf49e62c47dee26b462369d8cc75de6fa
Fakhri Alauddin <fakhricoffe@gmail.com>
2019-09-20 18:37:44 +0700

    tambah v2 dan perbaikan porfolio v1

---
+++ Website/send.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Tampilan </title>
+        <link rel="stylesheet" type="text/css" href="css\style.css">
+    <link rel="stylesheet" type="text/css" href="css\bulma.css">
+        <body>
+            
+        <div class= "container has-text-centered " id="contact">
+            <h1 class="title"> Terima Kasih </h1>
+        </div>
+        <br>
+        <br>
+  
+ 
+        <div class="container">
+  <div class="notification is-primary">
+    <div class="textpf has-text-justified is-family-monospace">
+   <h1> Name : <strong><?php echo  $_POST['Name']; ?></strong>
+   <h1> Telp : <strong><?php echo $_POST['Telp']; ?></strong> 
+   <h1> Email : <strong><?php echo $_POST['Email']; ?></strong> 
+   <h1> Your Massgae : <strong> <?php echo $_POST['Massage']; ?> </strong> 
+    </div> 
+  </div>
+</div>
+        </body>
+    </head>
+</html>
